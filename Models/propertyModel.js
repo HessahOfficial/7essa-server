94e15bda1224306e404e45145b75f4f49671e5d5
MohamedH782 <152233933+MohamedH782@users.noreply.github.com>
2025-01-21 03:16:26 +0200

    db config added , Property model added , eslint config added

---
+++ Models/propertyModel.js
@@ -0,0 +1,86 @@
+const mongoose = require('mongoose');
+
+const propertySchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'Property must have a title'],
+  },
+  description: {
+    type: String,
+    required: [true, 'Property must have a description'],
+  },
+  city: {
+    type: String,
+    required: [true, 'Property must have a city'],
+  },
+  locationLink: { type: String }, //the location link not required
+  size: {
+    type: Number,
+    required: [true, 'Property must have a size'],
+  },
+  numOfRooms: {
+    type: Number,
+    required: [true, 'number of rooms is required'],
+  },
+  images: {
+    type: [String],
+    required: [true, 'Property must have an image'],
+  },
+  //total shares always = 100?
+  totalShares: {
+    type: Number,
+    required: [true, 'total shares is required'],
+  },
+  availableShares: {
+    type: Number,
+    required: [true, 'available shares is required'],
+  },
+  yearlyPayment: {
+    type: Number,
+    required: [true, 'yearly Payment is required'],
+  },
+  price: {
+    type: [Number],
+    required: [true, 'Property must have a price'],
+  },
+  estimatedExitDate: {
+    type: Date,
+    required: [true, 'estimated exit date is required'],
+  },
+  isRented: {
+    type: Boolean,
+    default: false,
+  },
+  rentalIncome: {
+    type: Number,
+  },
+  rentalName: {
+    type: String,
+  },
+  rentalStartDate: {
+    type: Date,
+  },
+  rentalEndDate: {
+    type: Date,
+  },
+  benfits: {
+    type: Number,
+    required: [true, ''],
+  },
+  managementCompany: {
+    type: String,
+  },
+  status: {
+    type: String,
+    required: [true, 'property status is required'],
+    enum: ['Available', 'Funded', 'Exited'],
+  },
+  investmentDocs: {
+    type: String,
+    required: [
+      true,
+      'Property must have the investment documents',
+    ],
+  },
+  slug: String,
+});
--- server.js
+++ server.js
@@ -1,9 +1,19 @@
-const app = require('./App');
 const dotenv = require('dotenv');
-const express = require('express');
+const mongoose = require('mongoose');
 dotenv.config();
+const app = require('./App');
 const port = process.env.PORT;
+const DB = process.env.DATABASE_Hisham;
 
+mongoose
+  .connect(DB, {
+    // .connect(process.env.DATABASE_LOCAL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('DB connection successful');
+  });
 app.listen(port, () => {
   console.log(`App is running on port ${port} `);
 });
