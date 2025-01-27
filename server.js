3b4765015aec6efb47793a9bd95650c614dbd1a9
Mo7amed7osam <mido.hoss2003@gmail.com>
2025-01-27 13:34:07 +0200

    Fix: Added Monggose Config

--- server.js
+++ server.js
@@ -1,21 +1,12 @@
-const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const connectDB = require('./Config/dbConfig');
 
+const app = require('./app');
 dotenv.config();
-const app = require('./App');
+connectDB();
 
-const port = process.env.PORT;
-const DB = process.env.DATABASE_Hisham;
+const port = process.env.PORT || 3000;
 
-mongoose
-  .connect(DB, {
-    // .connect(process.env.DATABASE_LOCAL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log('DB connection successful');
-  });
 app.listen(port, () => {
-  console.log(`App is running on port ${port} `);
+  console.log(`App is running on port: ${port}`);
 });
