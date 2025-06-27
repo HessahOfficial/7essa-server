43f0c28dd5f65f6432c8d1e0ce347b4229de8bcd
Ahmed M. Ezzeldeen <Ahmed.Mohamed.Ezzeldeen@Gmail.com>
2025-06-27 23:11:20 +0300

    Refactor transaction schema to include propertyId and additional fields for shares and pricing

--- Models/TransactionModel.js
+++ Models/TransactionModel.js
@@ -1,33 +1,68 @@
 const mongoose = require('mongoose');
-const transactionSchema = new mongoose.Schema({
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true,
-    },
-    investmentId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Investment',
-        required: true,
-    },
-    transactionType: {
-        type: String,
-        enum: ['investing', 'selling'],
-        required: true,
-    },
-   
-    transactionDate: {
-        type: Date,
-        default: Date.now,
-    },
-    amount: {
-        type: Number,
 
-    },
-    status :{
-        type: String ,
-        enum: ['pending', 'completed', 'failed'],
-    }
-})
+const transactionSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  investmentId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Investment',
+    required: true,
+  },
+  propertyId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Property',
+    required: true,
+  },
+
+  transactionType: {
+    type: String,
+    enum: ['investing', 'selling'],
+    required: true,
+  },
+
+  numOfShares: {
+    type: Number,
+    required: true,
+  },
+
+  pricePerShareAtTransaction: {
+    type: Number,
+    required: true,
+  },
+
+  totalAmount: {
+    type: Number,
+    required: true,
+  },
+
+  status: {
+    type: String,
+    enum: ['pending', 'completed', 'failed'],
+    default: 'completed',
+  },
+
+  netGain: {
+    type: Number,
+    default: 0,
+  },
+
+  paymentMethod: {
+    type: String,
+    enum: ['instaPay', 'VodafoneCash', 'bankTransfer'],
+  },
+
+  adminNote: {
+    type: String,
+  },
+
+  transactionDate: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
 const Transaction = mongoose.model('Transaction', transactionSchema);
 module.exports = Transaction;
